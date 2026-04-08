dfdb7c6ac19dd4a70c303b40b4405476221e0f05
Kunal Agarwal <114900074+Kunalagarwal26@users.noreply.github.com>
2026-04-08 09:06:14 +0530

    Create Higher Than 75 Marks.sql

---
+++ SQL/Higher Than 75 Marks.sql
@@ -0,0 +1,8 @@
+-- Problem: Higher Than 75 Marks
+-- Link: https://www.hackerrank.com/challenges/more-than-75-marks/problem
+
+
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID;
