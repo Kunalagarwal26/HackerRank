67226b86822ecbaa6bed328e058bb842eb336d27
Kunal Agarwal <114900074+Kunalagarwal26@users.noreply.github.com>
2026-04-09 17:06:59 +0530

    Create Revising Aggregations - Averages.sql

---
+++ SQL/Revising Aggregations - Averages.sql
@@ -0,0 +1,7 @@
+-- Problem: Revising Aggregations - Averages
+-- Link: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
