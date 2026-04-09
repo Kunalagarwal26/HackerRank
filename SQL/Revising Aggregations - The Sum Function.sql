4a9c5a882217a17360bdb9ed170d974c0c5b8957
Kunal Agarwal <114900074+Kunalagarwal26@users.noreply.github.com>
2026-04-09 09:28:25 +0530

    Create Revising Aggregations - The Sum Function.sql

---
+++ SQL/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,7 @@
+-- Problem: Revising Aggregations - The Sum Function
+-- Link: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
